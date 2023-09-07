41a9017b9afbe6207cbeef6e43bda4af16e348a8
HichamBelazri <Hichambelazri>
2023-09-07 22:29:06 +0200

    xD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- *main - entry poit
+ * main - entry poit
  *
  * Description: A C program that prints with put function
  *
- *return: always 0 (sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
